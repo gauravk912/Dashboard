abe001b44550da37279d1e1c24917d34090d8e89
Gaurav Kulkarni <gykulkarni23@gmail.com>
2022-07-26 11:04:25 +0530

    Home Page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,10 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 import Widget from "../../components/widget/Widget";
+import Featured from "../../components/featured/Featured";
+import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
+// import { Table } from '@mui/material/Table';
 
 const Home = () => {
   return (
@@ -10,12 +14,21 @@ const Home = () => {
       <div className="homeContainer">
         <Navbar />
         <div className="widgets">
-          <Widget type="user"/>
-          <Widget type="order"/>
-          <Widget type="earning"/>
-          <Widget type="balance"/>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
         </div>
-        <div className="charts"></div>
+        <div className="charts">
+          <Featured />
+          <Chart />
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">
+            Latest Transaction
+          <Table/>
+          </div>
+        </div>
       </div>
     </div>
   );
